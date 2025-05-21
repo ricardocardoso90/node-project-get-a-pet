a5a00aa9c8e2a4df0b01c1618f701da618cd402a
ricardocardoso90 <ricardomanolo90@gmail.com>
2025-05-21 20:09:27 -0300

    update commit

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+
+app.get('/', (req, res) => {
+  // res.send("Olá mundão!!");
+  res.json({ message: "Deu tudo certo, Ricardo!!" });
+});
+
+app.listen(3000, () => console.log("Servidor rodando..."));
