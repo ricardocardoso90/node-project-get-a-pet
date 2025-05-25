a2d7e52e6382acf9f894d6ebf3248a2292ced161
ricardocardoso90 <ricardomanolo90@gmail.com>
2025-05-25 01:35:13 -0300

    update commit

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,9 +1,10 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-const createUserToken = require('../helpers/create-user-token');
+
 const getToken = require('../helpers/get-token');
 const getUserByToken = require('../helpers/get-user-by-token');
+const createUserToken = require('../helpers/create-user-token');
 
 module.exports = class UserController {
   static async register(req, res) {
@@ -12,68 +13,31 @@ module.exports = class UserController {
     const { name, email, password, confirmpassword, image, phone } = req.body;
 
     //VALIDANDO USUÁRIOS.
-    if (!name) {
-      res.status(422).json({ message: "O nome é obrigatório!!" });
-      return;
-    };
+    if (!name) { res.status(422).json({ message: "O nome é obrigatório!!" }); return; };
+    if (!email) { res.status(422).json({ message: "O email é obrigatório!!" }); return; };
 
-    if (!email) {
-      res.status(422).json({ message: "O email é obrigatório!!" });
-      return;
-    };
+    if (!password) { res.status(422).json({ message: "A senha é obrigatório!!" }); return; };
+    if (!confirmpassword) { res.status(422).json({ message: "A confirmação de senha é obrigatório!!" }); return; };
 
-    if (!password) {
-      res.status(422).json({ message: "A senha é obrigatório!!" });
-      return;
-    };
+    if (!image) { res.status(422).json({ message: "A imagem é obrigatório!!" }); return; };
+    if (!phone) { res.status(422).json({ message: "O Telefone é obrigatório!!" }); return; };
 
-    if (!confirmpassword) {
-      res.status(422).json({ message: "A confirmação de senha é obrigatório!!" });
-      return;
-    };
-
-    if (!image) {
-      res.status(422).json({ message: "A imagem é obrigatório!!" });
-      return;
-    };
-
-    if (!phone) {
-      res.status(422).json({ message: "O Telefone é obrigatório!!" });
-      return;
-    };
-
-    if (password !== confirmpassword) {
-      res.status(422).json({ message: "As senhas não são iguais!!" });
-      return;
-    };
+    if (password !== confirmpassword) { res.status(422).json({ message: "As senhas não são iguais!!" }); return; };
 
     //VERIFICAÇÃO SE O EMAIL JÁ FOI CADASTRADO.
     const userExists = await User.findOne({ email: email });
-    if (userExists) {
-      res.status(422).json(
-        { message: "Email já cadastrado por outro usuário!!" });
-      return;
-    };
+    if (userExists) { res.status(422).json({ message: "Email já cadastrado por outro usuário!!" }); return; };
 
     //CRIAÇÃO DA SENHA.
     const salt = await bcrypt.genSalt(12);
     const passwordHash = await bcrypt.hash(password, salt);
 
     //CRIAÇÃO DO USUÁRIO.
-    const user = new User({
-      name,
-      email,
-      password: passwordHash,
-      confirmpassword,
-      image,
-      phone
-    });
+    const user = new User({ name, email, password: passwordHash, confirmpassword, image, phone });
 
     try {
-
       const newUser = await user.save();
       await createUserToken(newUser, req, res);
-
     } catch (error) {
       res.status(500).json({ message: error });
     };
@@ -85,29 +49,17 @@ module.exports = class UserController {
     const { email, password } = req.body;
 
     //VALIDANDO USUÁRIOS.
-    if (!email) {
-      res.status(422).json({ message: "O email é obrigatório!!" });
-      return;
-    };
+    if (!email) { res.status(422).json({ message: "O email é obrigatório!!" }); return; };
 
-    if (!password) {
-      res.status(422).json({ message: "A senha é obrigatório!!" });
-      return;
-    };
+    if (!password) { res.status(422).json({ message: "A senha é obrigatório!!" }); return; };
 
     //VERIFICAÇÃO SE O EMAIL JÁ FOI CADASTRADO.
     const user = await User.findOne({ email: email });
-    if (!user) {
-      res.status(422).json({ message: "Não há usuário cadastrado com esse e-mail!!" });
-      return;
-    };
+    if (!user) { res.status(422).json({ message: "Não há usuário cadastrado com esse e-mail!!" }); return; };
 
     //VERIFICAÇÃO SE AS SENHAS SÃO IGUAIS AS JÁ CADASTRADAS.
     const checkPassword = await bcrypt.compare(password, user.password);
-    if (!checkPassword) {
-      res.status(422).json({ message: "As senhas não são iguais!!" });
-      return;
-    };
+    if (!checkPassword) { res.status(422).json({ message: "As senhas não são iguais!!" }); return; };
 
     await createUserToken(user, req, res);
   };
@@ -132,6 +84,7 @@ module.exports = class UserController {
     res.status(200).send(currentUser);
   };
 
+  //BUSCANDO USUÁRIO PELO ID.
   static async getUserById(req, res) {
     const id = req.params.id;
     const user = await User.findById(id).select('-password');
@@ -146,7 +99,7 @@ module.exports = class UserController {
     res.status(200).json({ user });
   };
 
-  //EDITAR USUÁRIOS.
+  //EDITANDO USUÁRIOS.
   static async editUser(req, res) {
 
     const token = getToken(req);
@@ -157,51 +110,21 @@ module.exports = class UserController {
     let image = '';
 
     //VALIDANDO USUÁRIOS.
-    if (!name) {
-      res.status(422).json({ message: "O nome é obrigatório!!" });
-      return;
-    };
-
-    if (!email) {
-      res.status(422).json({ message: "O email é obrigatório!!" });
-      return;
-    };
+    if (!name) { res.status(422).json({ message: "O nome é obrigatório!!" }); return; };
+    if (!email) { res.status(422).json({ message: "O email é obrigatório!!" }); return; };
 
     //CHECAR SE O EMAIL JÁ FOI CADASTRADO POR ALGUM USUÁRIO.
     const userExists = await User.findOne({ email: email });
-
-    if (user.email !== email && userExists) {
-      res.status(422).json({
-        message: "Por favor, utilize outro E-mail!!"
-      });
-      return;
-    };
+    if (user.email !== email && userExists) { res.status(422).json({ message: "Por favor, utilize outro E-mail!!" }); return; };
 
     user.email = email;
 
-    if (!password) {
-      res.status(422).json({ message: "A senha é obrigatório!!" });
-      return;
-    };
+    if (!password) { res.status(422).json({ message: "A senha é obrigatório!!" }); return; };
+    if (!confirmpassword) { res.status(422).json({ message: "A confirmação de senha é obrigatório!!" }); return; };
 
-    if (!confirmpassword) {
-      res.status(422).json({ message: "A confirmação de senha é obrigatório!!" });
-      return;
-    };
+    if (!image) { res.status(422).json({ message: "A imagem é obrigatório!!" }); return; };
+    if (!phone) { res.status(422).json({ message: "O Telefone é obrigatório!!" }); return; };
 
-    if (!image) {
-      res.status(422).json({ message: "A imagem é obrigatório!!" });
-      return;
-    };
-
-    if (!phone) {
-      res.status(422).json({ message: "O Telefone é obrigatório!!" });
-      return;
-    };
-
-    if (password !== confirmpassword) {
-      res.status(422).json({ message: "As senhas não são iguais!!" });
-      return;
-    };
+    if (password !== confirmpassword) { res.status(422).json({ message: "As senhas não são iguais!!" }); return; };
   };
 };
