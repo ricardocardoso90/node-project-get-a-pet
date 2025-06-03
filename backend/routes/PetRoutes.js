9b080d0a3c52432eef62a925d26a43698c1a3923
ricardocardoso90 <ricardomanolo90@gmail.com>
2025-06-03 15:36:03 -0300

    update commit

--- backend/routes/PetRoutes.js
+++ backend/routes/PetRoutes.js
@@ -2,9 +2,10 @@
 // const router = express.Router();
 
 const router = require('express').Router();
+const verifyToken = require('../helpers/verify-token');
 
 const PetController = require('../controllers/PetController');
 
-router.post('/create', PetController.create);
+router.post('/create', verifyToken, PetController.create);
 
 module.exports = router;
