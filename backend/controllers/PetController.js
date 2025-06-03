70bf4f459770c324ca29436863d69afda0e25039
ricardocardoso90 <ricardomanolo90@gmail.com>
2025-06-03 16:05:32 -0300

    update commit

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -1,5 +1,8 @@
 const Pet = require('../models/Pet');
 
+const getToken = require('../helpers/get-token');
+const getUserByToken = require('../helpers/get-user-by-token');
+
 module.exports = class PetController {
   static async create(req, res) {
 
@@ -7,11 +10,37 @@ module.exports = class PetController {
     const age = req.body.age;
     const weight = req.body.weight;
     const color = req.body.color;
-    const imagens = req.body.imagens;
+    const images = req.body.images;
 
-    const data = new Pet({ name, age, weight, color, imagens });
-    await data.save();
+    const available = true;
 
-    res.json({ message: "Dados cadastrados com sucesso!!" });
+    //VALIDAÇÕES.
+    if (!name) { res.status(422).json({ message: "O nome é obrigatório!!" }); return; }
+    if (!age) { res.status(422).json({ message: "A idade é obrigatória!!" }); return; }
+
+    if (!weight) { res.status(422).json({ message: "O peso é obrigatório!!" }); return; }
+    if (!color) { res.status(422).json({ message: "A cor é obrigatória!!" }); return; }
+    if (!images) { res.status(422).json({ message: "Adicione uma imgem!!" }); return; }
+
+    //USUÁRIO DONO DO PET.
+    const token = getToken(req);
+    const user = await getUserByToken(token);
+
+    //CRIAÇÃO E SALVAMENTO DE PETS.
+    const data = new Pet({
+      name, age, weight, color, available, images: [], user: {
+        _id: user._id,
+        name: user.name,
+        image: user.image,
+        phone: user.phone,
+      }
+    });
+
+    try {
+      const newPet = await data.save();
+      res.status(201).json({ message: "Pet cadastrado com sucesso!!", newPet });
+    } catch (error) {
+      res.status(500).json({ message: error });
+    };
   };
 };
