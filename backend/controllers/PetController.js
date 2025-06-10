6053349dd72f7984f9475f48a78813fea6e2cca8
Ricardo Cardoso <ricardomanolo90@gmail.com>
2025-06-10 17:53:34 -0300

    update commit

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -2,6 +2,7 @@ const Pet = require('../models/Pet');
 
 const getToken = require('../helpers/get-token');
 const getUserByToken = require('../helpers/get-user-by-token');
+const ObjectId = require('mongoose').Types.ObjectId;
 
 module.exports = class PetController {
 
@@ -25,6 +26,16 @@ module.exports = class PetController {
 
     const pets = await Pet.find({ 'adopter._id': user._id }).sort('-createdAt');
     res.status(200).json({ pets: pets });
+  };
+
+  static async getPetById(req, res) {
+    const id = req.params.id;
+
+    if (!ObjectId.isValid(id)) { res.status(422).json({ message: "ID Inválido!!" }); return; };
+    const pet = await Pet.findOne({ _id: id });
+
+    if (!pet) { res.status(404).json({ message: "Pet não encontrado!!" }) };
+    res.status(200).json({ pet: pet });
   };
 
   static async create(req, res) {
--- backend/routes/PetRoutes.js
+++ backend/routes/PetRoutes.js
@@ -7,6 +7,7 @@ const PetController = require('../controllers/PetController');
 router.get('/', PetController.getAll);
 router.get('/mypets', verifyToken, PetController.getAllUserPets);
 router.get('/myadoptions', verifyToken, PetController.myUserAdoptions);
+router.get('/:id', PetController.getPetById);
 
 router.post('/create', verifyToken, imageUpload.array('images'), PetController.create);
 
