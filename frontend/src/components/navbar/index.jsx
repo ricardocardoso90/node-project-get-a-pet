468f7944e60e0e98e47782a780367bbff9b29627
Ricardo Cardoso <ricardomanolo90@gmail.com>
2025-07-01 17:26:01 -0300

    update commit

--- frontend/src/components/navbar/index.jsx
+++ frontend/src/components/navbar/index.jsx
@@ -1,8 +1,15 @@
 import { Link } from "react-router-dom";
 
+import Logo from "../../assets/img/logo.png";
+
 export function Navbar() {
   return (
     <nav>
+      <div>
+        <img src={Logo} alt="Get A Pet" />
+        <h2>Get A Pet</h2>
+      </div>
+
       <ul>
         <li><Link to="/">Adotar</Link></li>
         <li><Link to="/login">Entrar</Link></li>
