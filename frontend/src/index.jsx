1853cb87cfb83eeb67af5c10b190e4069f0ca9de
Ricardo Cardoso <ricardomanolo90@gmail.com>
2025-07-01 14:51:30 -0300

    update commit

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import './index.css';
 import App from './App';
+import './index.css';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
