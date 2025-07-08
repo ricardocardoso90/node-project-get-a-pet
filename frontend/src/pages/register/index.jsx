35a94d651d32322c1dc4d571d4acd3e316e33055
Ricardo Cardoso <ricardomanolo90@gmail.com>
2025-07-08 13:45:37 -0300

    update commit

--- frontend/src/pages/register/index.jsx
+++ frontend/src/pages/register/index.jsx
@@ -1,5 +1,57 @@
+import { Input } from "../../components/input";
+
 export function Register() {
+  function handleChange(e) {
+    console.log(e.target.value);
+  };
+
   return (
-    <h1>Página REGISTER!</h1>
+    <section>
+      <h1>Registrar</h1>
+
+      <form>
+        <Input
+          text="Nome"
+          type="text"
+          name="name"
+          placeholder="Digite o seu nome"
+          handleOnChange={handleChange}
+        />
+
+        <Input
+          text="Telefone"
+          type="text"
+          name="phone"
+          placeholder="Digite o seu telefone"
+          handleOnChange={handleChange}
+        />
+
+        <Input
+          text="E-mail"
+          type="email"
+          name="email"
+          placeholder="Digite o seu E-mail"
+          handleOnChange={handleChange}
+        />
+
+        <Input
+          text="Senha"
+          type="password"
+          name="password"
+          placeholder="Digite a sua Senha"
+          handleOnChange={handleChange}
+        />
+
+        <Input
+          text="Confirmação de Senha"
+          type="password"
+          name="confirmpassword"
+          placeholder="Confirme a sua Senha"
+          handleOnChange={handleChange}
+        />
+
+        <input type="submit" value="Cadastrar" />
+      </form>
+    </section>
   );
 };
