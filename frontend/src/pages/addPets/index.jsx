e4223bd5202ee87a87e453e78903bff5c26f5fbf
Ricardo Cardoso <ricardomanolo90@gmail.com>
2025-07-18 21:07:08 -0300

    update commit

---
+++ frontend/src/pages/addPets/index.jsx
@@ -0,0 +1,12 @@
+import styles from "./styles.module.css";
+
+export function AddPets() {
+
+  return (
+    <section>
+      <div>
+        <h1>Adicionar Pets</h1>
+      </div>
+    </section>
+  );
+};
--- frontend/src/routes/auth.routes.jsx
+++ frontend/src/routes/auth.routes.jsx
@@ -5,6 +5,7 @@ import { Login } from "../pages/login";
 import { Register } from "../pages/register";
 import { Profile } from "../pages/profile";
 import { MyPets } from "../pages/myPets";
+import { AddPets } from "../pages/addPets";
 
 export function AuthRoutes() {
   return (
@@ -14,6 +15,7 @@ export function AuthRoutes() {
       <Route path="/register" element={<Register />} />
       <Route path="/user/profile" element={<Profile />} />
       <Route path="/pet/mypets" element={<MyPets />} />
+      <Route path="/pet/add" element={<AddPets />} />
     </Routes>
   );
 };
