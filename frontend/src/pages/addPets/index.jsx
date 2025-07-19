d90efecd8d0137968eef3d3ec9307b94c39cc2db
Ricardo Cardoso <ricardomanolo90@gmail.com>
2025-07-18 21:25:53 -0300

    update commit

--- frontend/src/pages/addPets/index.jsx
+++ frontend/src/pages/addPets/index.jsx
@@ -1,12 +1,14 @@
 import styles from "./styles.module.css";
 
 export function AddPets() {
-
   return (
-    <section>
+    <section className={styles["addpet-header"]}>
       <div>
-        <h1>Adicionar Pets</h1>
+        <h1>Cadastre um Pet</h1>
+        <p>Depois ele ficará disponível para adoção.</p>
       </div>
+
+      <p>Formulário</p>
     </section>
   );
 };
