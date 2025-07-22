bee302d1a24fbf2baf3df82f54e896e071ab60d0
Ricardo Cardoso <ricardomanolo90@gmail.com>
2025-07-21 21:34:48 -0300

    update commit

--- frontend/src/components/petForm/index.jsx
+++ frontend/src/components/petForm/index.jsx
@@ -12,19 +12,38 @@ export function PetForm({ petData, btnText, handleSubmit }) {
   const colors = ["Branco", "Preto", "Cinza", "Caramelo", "Mesclado"];
 
   function onFileChange(e) {
-
+    setPreview(Array.from(e.target.files));
+    setPet({ ...pet, images: [...e.target.files] });
   };
 
   function handleChange(e) {
-
+    setPet({ ...pet, [e.target.name]: e.target.value });
   };
 
   function handleColor(e) {
+    setPet({ ...pet, color: e.target.options[e.target.selectedIndex].text });
+  };
 
+  function submit(e) {
+    e.preventDefault();
+    // handleSubmit(pet);
+    console.log(pet);
   };
 
   return (
-    <form className={stylesGlobals["form-container"]}>
+    <form onSubmit={submit} className={stylesGlobals["form-container"]}>
+      <div className={stylesGlobals["preview-pet-images"]}>
+        {preview.length > 0
+          ? preview.map((image, index) => (
+            <img src={URL.createObjectURL(image)} alt={pet.name} key={`${pet.name}+${index}`} />
+          ))
+          : pet.images &&
+          pet.images.map((image, index) => (
+            <img src={`${process.env.REACT_APP_API}/images/pets/${image}`} alt={pet.name} key={`${pet.name}+${index}`} />
+          ))
+        }
+      </div>
+
       <Input
         type="file"
         name="images"
